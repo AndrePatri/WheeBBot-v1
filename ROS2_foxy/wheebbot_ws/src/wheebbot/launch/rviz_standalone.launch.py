ead2c1b00c12499162c3edaff1220cc77a77868b
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-01-03 15:09:18 +0100

    successfull joint state publisher from gazebo to rviz

--- ROS2_foxy/wheebbot_ws/src/wheebbot/launch/rviz_standalone.launch.py
+++ ROS2_foxy/wheebbot_ws/src/wheebbot/launch/rviz_standalone.launch.py
@@ -2,35 +2,36 @@ from ament_index_python.packages import get_package_share_path
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-
 from launch.substitutions import Command, LaunchConfiguration
 
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-
+    
     package_share_path = get_package_share_path('wheebbot')
     default_model_path = package_share_path/'description/urdf/wheebbot.urdf.xacro'
     default_rviz_config_path = package_share_path/'rviz/wheebbot.rviz'
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    use_sim_time_arg=DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation (Gazebo) clock if true')
 
     model_arg = DeclareLaunchArgument(name='model', default_value=str(default_model_path),
                                       description='Absolute path to robot urdf file')
     rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
                                      description='Absolute path to rviz config file')
-    is_sim_time_arg= DeclareLaunchArgument(name='use_sim_time',
-            default_value='false',
-            description='Use simulation (Gazebo) clock if true')
 
-    robot_description = ParameterValue(Command(['xacro', LaunchConfiguration('model')]),
+    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
                                        value_type=str)
-                                    
 
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        name='robot_state_publisher',
         parameters=[{'use_sim_time': use_sim_time,'robot_description': robot_description}]
     )
 
@@ -40,12 +41,13 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
+        parameters=[{'use_sim_time': use_sim_time}]
     )
 
     return LaunchDescription([
         model_arg,
         rviz_arg,
-        is_sim_time_arg,
+        use_sim_time_arg,
         robot_state_publisher_node,
-        rviz_node
+        rviz_node,
     ])
